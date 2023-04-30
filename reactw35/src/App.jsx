89cea724decfc9a03bef430533b0b9bf8ffc3d52
Oksanaka848 <okmitka@mail.ru>
2023-04-30 18:04:50 +0300

    pretty formated app.jsx

--- reactw35/src/App.jsx
+++ reactw35/src/App.jsx
@@ -1,66 +1,59 @@
-import React, {  Component } from 'react';
+import React, { Component } from 'react';
 import './App.css'
 
 export default class App extends Component {
-
   state = {
     comments: [],
+    active: null,
     form: {
       name: '',
       comment: ''
     }
   }
-
-    addComment = (e) => {
-      e.preventDefault();
+  target(id) {
+    this.setState({ active: id });
+  }
+  addComment = (e) => {
+    e.preventDefault();
     this.setState({
       comments: [
-        
         {
           name: this.state.form.name,
           comment: this.state.form.comment
-          
-        } ,
+        },
         ...this.state.comments
-      ],
+      ], active: +e.target.classList.contains("new_comments"),
       form: {
         name: '',
         comment: ''
       }
-      
     })
   }
-
   removeComment = (id) => {
     this.setState({
       comments: this.state.comments.filter(comment => comment.id !== id)
-    })}
-
+    })
+  }
   handleChange = (e) => {
-
     console.log(e.target.name)
-    
     this.setState({
       form: {
         ...this.state.form,
         [e.target.name]: e.target.value,
-        
       }
     })
   }
-
-    
-
   render() {
+    const { comment, active } = this.state;
     return (
       <div className="App">
-        {this.state.comments.map(comment => <div key={comment.id}>
+        {this.state.comments.map((comment, i) => <div key={comment.id} className='comment_box'>
           <span>{comment.id} </span>
           <div className='user_id'>
-                    <a href="https://pixelbox.ru/wp-content/uploads/2022/06/risovanie-avatars-for-girls-pixelbox.ru-54.jpg"><img src="https://pixelbox.ru/wp-content/uploads/2022/06/risovanie-avatars-for-girls-pixelbox.ru-54.jpg" width="50" 
-  height="50" alt="avatar"/></a>
-  <span>   {comment.name}</span></div>
-          <div className="comments">{comment.comment}</div>
+            <a href="https://pixelbox.ru/wp-content/uploads/2022/06/risovanie-avatars-for-girls-pixelbox.ru-54.jpg"><img src="https://pixelbox.ru/wp-content/uploads/2022/06/risovanie-avatars-for-girls-pixelbox.ru-54.jpg" width="50"
+              height="50" alt="avatar" /></a>
+            <span>   {comment.name}</span></div>
+          <div className={i === active ? 'new_comments' : 'comments'}>{comment.comment}</div>
           <button className="btn_del" onClick={this.removeComment.bind(null, comment.id)}>Удалить комментарий</button>
         </div>)}
         <div className="form-name">
@@ -80,4 +73,3 @@ export default class App extends Component {
     )
   }
 }
-
